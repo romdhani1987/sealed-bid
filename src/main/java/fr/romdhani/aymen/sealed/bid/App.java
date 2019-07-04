f4305e3e996146d6d477023ba167ef6cc3501d46
Romdhani <romdhani@live.fr>
2019-07-04 23:20:54 +0200

    [sealed-bid] initial import

---
+++ src/main/java/fr/romdhani/aymen/sealed/bid/App.java
@@ -0,0 +1,13 @@
+package fr.romdhani.aymen.sealed.bid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
