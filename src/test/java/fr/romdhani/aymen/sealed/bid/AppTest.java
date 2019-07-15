028908282dc8c1a5295541620ffc749248529ebc
Romdhani <romdhani@live.fr>
2019-07-15 15:19:26 +0200

    [sealed-bid] initial import

--- src/test/java/fr/romdhani/aymen/sealed/bid/AppTest.java
+++ src/test/java/fr/romdhani/aymen/sealed/bid/AppTest.java
@@ -1,5 +1,7 @@
 package fr.romdhani.aymen.sealed.bid;
 
+import fr.romdhani.aymen.sealed.bid.model.Buyer;
+import fr.romdhani.aymen.sealed.bid.model.ObjectToSale;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +9,57 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Sealed-bid test
+	 */
+	public void testSealedBidAuction() {
+		// The potential buyers
+		Buyer A = new Buyer("A");
+		assertTrue(A != null);
+		System.out.println(A.toString() + " is a potential buyer!");
+		Buyer B = new Buyer("B");
+		assertTrue(B != null);
+		System.out.println(B.toString() + " is a potential buyer!");
+		Buyer C = new Buyer("C");
+		assertTrue(C != null);
+		System.out.println(C.toString() + " is a potential buyer!");
+		Buyer D = new Buyer("D");
+		assertTrue(D != null);
+		System.out.println(D.toString() + " is a potential buyer!");
+		Buyer E = new Buyer("E");
+		assertTrue(E != null);
+		System.out.println(E.toString() + " is a potential buyer!");
+		// The object to sale
+		ObjectToSale objectToSale = new ObjectToSale(100D);
+		assertTrue(objectToSale != null);
+		System.out.println(objectToSale.toString());
+		// Auction
+		Auction auction = new Auction(objectToSale);
+		// Set the potential buyer to the auction
+		auction.addBuyer(A);
+		auction.addBuyer(B);
+		auction.addBuyer(C);
+		auction.addBuyer(C);
+		auction.addBuyer(E);
+		System.out.println(auction.toString());
+		
+	}
 }
